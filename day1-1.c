886deef9df112efcf274750c15bd8d837aa630d0
Ambika-nangru <Ambika.32188@stu.upes.ac.in>
2026-08-17 22:53:11 +0530

    day 1 codes

---
+++ day1-1.c
@@ -0,0 +1,19 @@
+/*
+NAME-AMBIKA
+SAP ID-590032188]
+BATCH NO-24
+
+QUESTION:
+WRITE A PROGRAM TO INPUT TWO NOD ND THEIR SUM
+*/
+
+#include <stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter any two numbers");
+scanf("%d %d",&a,&b);
+sum = a+b;
+printf("sum= %d\n",sum);
+return 0;
+}
